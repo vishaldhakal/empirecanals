8954f0ed433493ac0e868d2bda112f3710babb91
vishaldhakal <vishaldhakal96@gmail.com>
2024-03-05 23:43:56 +0545

    d

--- app/api/sendEmail/route.js
+++ app/api/sendEmail/route.js
@@ -1,17 +1,17 @@
 // Write me a nodemailer email sending code for api to send email to stmp server
 
 import { NextRequest, NextResponse } from "next/server";
-
 import { Resend } from "resend";
+
 const resend = new Resend("re_mYeVPcTd_4gSkQaE4s2Mmd1Jb5XGyK9tK");
 
-export async function POST(req: NextRequest, res: NextResponse) {
+export async function POST(req, res) {
   try {
     let body = await req.json();
 
     const { data, error } = await resend.emails.send({
       from: "Inquiry <info@empirecanalhomes.ca>",
-      to: ["vishaldhakal96@gmail.com"],
+      to: ["Salimanazeer@hotmail.com"],
       subject: "Inquiry About Welland Canals Landing Page",
       text: `Name: ${body.name}\nEmail: ${body.email} \nPhone: ${body.phone} \nMessage: ${body.message} \n`,
     });
